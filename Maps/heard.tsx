ee856ad2267cfbf6f8c168e3e86ee611c2302800
Dozhder <zyablicev.dk1@gmail.com>
2026-02-03 19:55:44 +0500

    Delete Maps directory

--- Maps/heard.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="heard" tilewidth="64" tileheight="64" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="3">
-  <image source="../Desktop/hud_heart.png" width="64" height="64"/>
- </tile>
-</tileset>
